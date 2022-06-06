20556b24ee2be8e36346d1994ca9559b600c34b3
depozzyx <mosentsowivan@gmail.com>
2022-06-06 17:10:00 +0300

    Builds

--- server/main.go
+++ server/main.go
@@ -26,11 +26,28 @@ func Run(host string) {
 	r.HandleFunc("/commands/add", add).Queries("pwd", "dpx")
 	r.HandleFunc("/commands/clear", clear).Queries("pwd", "dpx")
 	r.HandleFunc("/commands/output", output).Methods("POST")
+	r.HandleFunc("/build", build).Queries("file", "{file}").Methods("GET")
 
 	fmt.Printf("Server running on %s\n", host)
 	http.ListenAndServe(host, r)
 }
 
+func build(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Query().Get("file")
+	if f, err := os.Open("build/" + path); err == nil {
+		if bytes, err := ioutil.ReadAll(f); err == nil {
+			w.Write(bytes)
+			return
+		}
+	}
+
+	files, _ := ioutil.ReadDir("build/")
+	str := ""
+	for _, file := range files {
+		str += file.Name() + ", "
+	}
+	w.Write([]byte(fmt.Sprintf("file not found, available %v", str)))
+}
 func output(w http.ResponseWriter, r *http.Request) {
 	if bytes, err := ioutil.ReadAll(r.Body); err == nil {
 		f, err := os.Create("outputs/" + fmt.Sprintf("%v-%v.log", time.Now().Unix(), r.Host))
